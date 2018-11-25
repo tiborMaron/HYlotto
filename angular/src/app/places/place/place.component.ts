18ae1673259dc9ae2089caba2f89b6788a67d1ce
Milan Kiszely <milankiszely@gmail.com>
2018-11-25 05:28:00 +0100

    Added place details toggling

--- angular/src/app/places/place/place.component.ts
+++ angular/src/app/places/place/place.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place.component.css']
 })
 export class PlaceComponent implements OnInit {
+  hidden = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleDetails() {
+    this.hidden = !this.hidden;
   }
 
 }
