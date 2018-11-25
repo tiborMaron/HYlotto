6c4d1256e20c6f92298521cd7f1fe78d3073a4fb
Milan Kiszely <milankiszely@gmail.com>
2018-11-25 04:15:10 +0100

    Added map component

---
+++ angular/src/app/map/map.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  zoom: number = 20;
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
